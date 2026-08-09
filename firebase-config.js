388f5d3fe093f56ef3d49fec50a5960b16f94b5a
adhamhasy <122464685+adhamhasy@users.noreply.github.com>
2026-08-09 22:32:35 +0300

    Update firebase-config.js imports

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 // firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { db, ref, onChildAdded, onChildChanged, onChildRemoved, push, set } from "./firebase-config.js";
 import {
   getDatabase,
   ref,
